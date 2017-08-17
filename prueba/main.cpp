20a35a8a8654043c4ea70eafd641537f4264f866
ALUMNO-D <ALUMNO-D@UCSP.AULAB>
2017-08-17 11:03:56 -0500

    first commit

---
+++ prueba/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1;
+    int num2;
+    int resultado;
+    cout << "Ingrese el primer numero\n";
+    cin >> num1;
+    cout << "Ingrese el segundo numero\n";
+    cin >> num2;
+    resultado = num1 + num2;
+    cout << resultado << "\n";
+  return 0;
+}
