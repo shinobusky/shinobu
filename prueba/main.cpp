6149ac50c9556cad4f9d3c657715589d1c8ac27e
ALUMNO-D <ALUMNO-D@UCSP.AULAB>
2017-08-17 11:10:12 -0500

    first commit

--- prueba/main.cpp
+++ prueba/main.cpp
@@ -6,12 +6,15 @@ int main()
 {
     int num1;
     int num2;
+	int num3;
     int resultado;
     cout << "Ingrese el primer numero\n";
     cin >> num1;
     cout << "Ingrese el segundo numero\n";
     cin >> num2;
-    resultado = num1 + num2;
+	cout << "Ingrese el tercer numero\n";
+	cin >> num3;
+    resultado = num1 + num2 + num3;
     cout << resultado << "\n";
   return 0;
 }
